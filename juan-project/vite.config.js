c188207c412089b848baf0dda4f4b33fcc35121d
jaad02 <juanantonioaznar2002@gmail.com>
2025-06-02 19:54:58 +0200

    Fix build y puerto para despliegue en Railway

--- juan-project/vite.config.js
+++ juan-project/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: '/', // importante si usas React Router (puedes quitar si no lo usas)
 })
